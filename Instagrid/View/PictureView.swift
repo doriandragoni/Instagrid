1e7f25eb4da62b33471189a5d1b915e32afc0410
Dorian Dragoni <doriandragoni@icloud.com>
2022-05-29 14:13:11 +0200

    Create a custom class to use for the pictures in the grid view

---
+++ Instagrid/View/PictureView.swift
@@ -0,0 +1,56 @@
+//
+//  PictureView.swift
+//  Instagrid
+//
+//  Created by Dorian Dragoni on 29/05/2022.
+//
+
+import UIKit
+
+@IBDesignable
+class PictureView: UIView {
+    let nibName = "PictureView"
+    var contentView: UIView?
+    
+    @IBOutlet weak var plusIcon: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        view.frame = self.bounds
+        self.addSubview(view)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewTapped(tapGestureRecognizer:)))
+            view.isUserInteractionEnabled = true
+            view.addGestureRecognizer(tapGestureRecognizer)
+        
+        contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
+    @objc func viewTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+//        let tappedView = tapGestureRecognizer.view as! UIView
+
+        // Your action
+        if plusIcon.isHidden {
+            plusIcon.isHidden = false
+        } else {
+            plusIcon.isHidden = true
+        }
+    }
+}
